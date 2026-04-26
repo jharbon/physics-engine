70524bf19f99edfe2b82477518f1d3079ef13721
James Harbon <jharbon299@gmail.com>
2026-04-26 14:38:10 +0100

    Render window using GLFW
    - Update CMakeLists to find and link OpenGL and GLFW.
    - Create OpenGL window context and render in loop until closed.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,37 @@
+#include <GLFW/glfw3.h>
 #include <iostream>
 
 int main(int argc, char* argv[]) {
-    std::cout << "Physics engine" << std::endl;
+    if (!glfwInit()) {
+        std::cerr << "Failed to initialise GLFW" << std::endl;
+        return -1;
+    }
+
+    GLFWwindow* window = glfwCreateWindow(
+        960,  // Width
+        540,  // Height
+        "Physics Engine",  // Title
+        NULL,  // Windowed mode
+        NULL  // Do not share resources
+    );
+
+    if (!window) {
+        std::cerr << "Failed to initialise window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    // Make all OpenGL commands affect window
+    glfwMakeContextCurrent(window);
+
+    // Run main loop until window is closed
+    while (!glfwWindowShouldClose(window)) {
+        // Check for new events
+        glfwPollEvents();
+        // Display frame
+        glfwSwapBuffers(window);
+    }
+
+    glfwTerminate();
     return 0;
 }
