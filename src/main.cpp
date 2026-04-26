a4af3b68e29b543ecd21b3e92223a4a82b6e180f
James Harbon <jharbon299@gmail.com>
2026-04-26 19:59:19 +0100

    Render triangle on screen
    - Create vertices, vertex buffer object, and vertex array object.
    - Upload vertex data from CPU to GPU and specify how to interpret it.
    - Define, compile, and link shaders to process positions and set pixel colours.
    - Use shader program to draw triangle in main loop.

--- src/main.cpp
+++ src/main.cpp
@@ -33,6 +33,74 @@ int main(int argc, char* argv[]) {
 
     std::cout << "OpenGL version: " << glGetString(GL_VERSION) << std::endl;
 
+    float vertices[] = {
+        0.0f,  0.5f,   // top
+        -0.5f, -0.5f,   // bottom left
+        0.5f, -0.5f    // bottom right      
+    };
+
+    // Generate and bind Vertex Array Object (intepret data) and Vertex Buffer Object (store data on GPU)
+    unsigned int VAO, VBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    // Upload vertex data from CPU to GPU; copies vertices array into VBO
+    glBufferData(
+        GL_ARRAY_BUFFER,  // Target
+        sizeof(vertices), 
+        vertices,  // Pointer to CPU-side data 
+        GL_STATIC_DRAW
+    );
+    // Tell GPU how to interpret vertex data
+    glVertexAttribPointer(
+        0,  // Attribute location (see shader)
+        2,  // Values per vertex (x,y)
+        GL_FLOAT,  // Value type
+        GL_FALSE,  // Do not normalise (values already in correct range)
+        2 * sizeof(float),  // Size of vertex
+        (void*)0  // Where attribute starts in buffer
+    );
+    glEnableVertexAttribArray(0);
+
+    // Shader to process positions
+    const char* vertexShaderSource = R"(
+    #version 330 core
+    layout (location = 0) in vec2 aPos;
+
+    void main() {
+        gl_Position = vec4(aPos, 0.0, 1.0);
+    }
+    )";
+    // Shader to set pixel colour
+    const char* fragmentShaderSource = R"(
+    #version 330 core
+    out vec4 FragColor;
+
+    void main() {
+        FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+    }
+    )";
+
+    // Compile and link shaders:
+
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+    unsigned int shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+
     // Run main loop until window is closed
     while (!glfwWindowShouldClose(window)) {
         // Check for new events
@@ -41,6 +109,11 @@ int main(int argc, char* argv[]) {
         // Clear screen to RGBA colour
         glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        // Use shader to draw triangle
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         // Display frame
         glfwSwapBuffers(window);
