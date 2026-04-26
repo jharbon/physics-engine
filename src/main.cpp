8d0a8f4dcb969dc4f13ec257a232509747e3187e
James Harbon <jharbon299@gmail.com>
2026-04-26 15:40:26 +0100

    Test OpenGL functions using GLAD
    - Add GLAD headers and source.
    - Update CMakeLists to use include directory and compile GLAD source.
    - Load pointers to OpenGL functions from driver into GLAD.
    - Clear window screen to RGBA colour.

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
 
@@ -24,10 +25,23 @@ int main(int argc, char* argv[]) {
     // Make all OpenGL commands affect window
     glfwMakeContextCurrent(window);
 
+    // Load pointers to OpenGL functions from driver into GLAD
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cerr << "Failed to initialise GLAD" << std::endl;
+        return -1;
+    }
+
+    std::cout << "OpenGL version: " << glGetString(GL_VERSION) << std::endl;
+
     // Run main loop until window is closed
     while (!glfwWindowShouldClose(window)) {
         // Check for new events
         glfwPollEvents();
+
+        // Clear screen to RGBA colour
+        glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
         // Display frame
         glfwSwapBuffers(window);
     }
